caa5866486a0b69253f12a7d2474ddd40c128b34
João Álvaro Oliveira Silva <91163460+JoaoAlv4ro@users.noreply.github.com>
2024-08-30 16:03:16 -0300

    Delete LISTA III - João Álvaro - PPZ directory

--- LISTA III - João Álvaro - PPZ/numero 2.py
+++
@@ -1,9 +0,0 @@
-# Faça um programa que leia um nome de usuário e a sua senha e não aceite a senha igual ao
-# nome do usuário, mostrando uma mensagem de erro e voltando a pedir as informações.
-
-username = str(input('Digite um nome de usuário: '))
-password = str(input('Digite uma senha: '))
-while username == password:
-    print('Erro! Nome e senha de seu usuário não podem ser iguais.')
-    username = str(input('Digite um nome de usuário: '))
-    password = str(input('Digite uma senha: '))
